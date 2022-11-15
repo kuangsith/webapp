0be99adabf5c6a6eb552ca8e8187168a1d23df0f
kuangsith <108988177+kuangsith@users.noreply.github.com>
2022-11-15 10:09:41 +0700

    Update stone.py

--- stone.py
+++ stone.py
@@ -14,3 +14,9 @@ n = 15
 
 st.write("The number is "+str(n))
 st.latex(r'''x = \frac{-b\pm\sqrt{b^2-4ac}{2a}}''')
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
