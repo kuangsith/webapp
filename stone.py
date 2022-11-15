2b024e22c9e4a9da4d455e55aad6fae07badd347
kuangsith <108988177+kuangsith@users.noreply.github.com>
2022-11-15 21:29:37 +0700

    Update stone.py

--- stone.py
+++ stone.py
@@ -21,11 +21,11 @@ if minus1:
     st.session_state.count = st.session_state.count-1
 
 st.write("The number is ")
-st.title(str(n))
-st.latex(r'''x = \frac{-b\pm\sqrt{b^2-4ac}}{2a}''')
+st.title(str(st.session_state.count))
+#st.latex(r'''x = \frac{-b\pm\sqrt{b^2-4ac}}{2a}''')
 
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+#st.latex(r'''
+#    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+#    \sum_{k=0}^{n-1} ar^k =
+#    a \left(\frac{1-r^{n}}{1-r}\right)
+#    ''')
